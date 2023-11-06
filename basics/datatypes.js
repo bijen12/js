54bcd92c19d087ecce37ded754bf598c50fb3d11
Bijen Singha <76146952+bijen12@users.noreply.github.com>
2023-11-06 17:23:12 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) // we are using node js, not browser
+console.log("6")
+
+let name="bijen"
+let age=18
+let isloggedIn=false
+
+//number=2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+console.log(typeof undefined);//undefined
+console.log(typeof undefined);//object
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail="bijensingha@gmail.com"
+var accountPassword="12345"
+accountCity="silchar"
+//accountId=2
+console.log(accountId)
+accountEmail="bijen@gmail.com"
+accountPassword="11111"
+accountCity="bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*prefer not to us var because of issue in scope*/
