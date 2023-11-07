cb0433e000912cb8fab039b80606f9398e61c02e
Bijen Singha <76146952+bijen12@users.noreply.github.com>
2023-11-07 07:18:14 +0000

    slice and splice

---
+++ basics/arrays.js
@@ -0,0 +1,20 @@
+//array
+const arr=[0,1,2,3,4,5,true,'bijen']
+console.log(arr[0]);
+const heroes=["saktiman","spiderman"]
+const array=new Array(1,2,3,4,5)
+console.log(array[1]);
+array.push(6)
+console.log(array);
+array.unshift(9)
+array.shift()
+const newarray=array.join()
+
+//slice splice
+
+
+
+a=[1,2,3,4,5]
+console.log(a.slice(1,3));
+console.log(a.splice(1,3));
+console.log(a);
